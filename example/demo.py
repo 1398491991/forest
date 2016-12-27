6456f7001bd1bb7a718e5bf3e6d55e5b26be83bf
zzl <zhanzl@i2dd.com>
2016-12-27 16:22:30 +0800

    test

--- example/demo.py
+++ example/demo.py
@@ -1,16 +1,20 @@
 # import scrapy
-# from forest.http import Request
-from forest.core.scheduler.scheduler import process_request
-# import pickle
+from forest.decorator.spider import delay
+from forest.spiders import Spider
 
-class a:
-    pass
+class Demo(Spider):
 
-class test(object):
-    def run1(self,response=None):
-        print response,'####################'
-        process_request.delay({'url':'http://www.baidu.com','callback':'run2','self':self.__class__})
+
+    @delay
+    def run1(self):
+        return [{'url':'http://www.baidu.com',
+                 'callback':'run2',
+                 'self':self},]*10
 
     def run2(self,response):
-        print response,'@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
-        process_request.delay({'url':'http://www.baidu.com','callback':'run1','self':self.__class__})
+        print '#################  ',response
+
+
+
+
+
--- example/demo2.py
+++ example/demo2.py
@@ -1,3 +1,17 @@
-from demo import test
-t=test()
-t.run1()
+from demo import Demo
+
+d=Demo()
+d.run1()
+
+#
+# from scrapy import Request
+# class a(object):
+#     def run1(self):
+#         import pickle
+#         pickle.dumps(Request('http://www.baidu.com',callback=self.run2))
+#
+#
+#     def run2(self):
+#         pass
+#
+# a().run1()
